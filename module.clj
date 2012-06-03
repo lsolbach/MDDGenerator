48d7f86bd50dcc89c43c4be4e0c189811ca019b6
Ludger Solbach <soulman@soulspace.org>
2012-06-03 16:42:23 +0000

    added baumeister support

---
+++ module.clj
@@ -0,0 +1,11 @@
+[
+ :name "UmlModelBuilder"
+ :type "library"
+ :version "0.2.0"
+ :description "A builder for generator models from UML 1.4/XMI 1.2 models"
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "java" "archive" "publish"]
+ :dependencies [["org.soulspace" "XmiRepository2" "0.2.0"]
+                ["org.soulspace" "ModelRepository2" "0.2.0"]
+                ["org.junit" "junit" "3.8.1" "dev"]]
+ ]
