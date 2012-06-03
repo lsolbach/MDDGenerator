1e6e94f0b205df229ae4ddbe331d5b1a31f99ed8
Ludger Solbach <soulman@soulspace.org>
2012-06-03 15:58:58 +0000

    added baumeister support

---
+++ module.clj
@@ -0,0 +1,9 @@
+[
+ :name "ModelRepository2"
+ :type "library"
+ :version "0.2.0"
+ :description "A domain model for the generator framework"
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "java" "archive" "publish"]
+ :dependencies [["org.junit" "junit" "3.8.1" "dev"]]
+ ]
