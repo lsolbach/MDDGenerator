196e091b3a59810839e158c8a122384d0883d736
Ludger Solbach <soulman@soulspace.org>
2012-06-03 16:45:24 +0000

    added baumeister support, enhanced reader template

---
+++ module.clj
@@ -0,0 +1,9 @@
+[
+ :name "XmiRepository2"
+ :type "library"
+ :version "0.2.0"
+ :description "A repository for UML 1.4/XMI 1.2 files"
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "java" "archive" "publish"]
+ :dependencies [["org.junit" "junit" "3.8.1" "dev"]]
+ ]
