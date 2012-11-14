9ed7ea9e0c7e8a18019d8f8b450f047325636fb4
Ludger Solbach <soulman@soulspace.org>
2012-11-14 10:21:01 +0000

    updated module.clj

--- module.clj
+++ module.clj
@@ -1,9 +1,10 @@
 [
  :name "XmiRepository2"
+ :project "org.soulspace.modelling"
  :type "library"
  :version "0.2.0"
  :description "A repository for UML 1.4/XMI 1.2 files"
  :vendor "soulspace.org"
- :plugins ["global" "deps" "java" "archive" "publish"]
- :dependencies [["org.junit" "junit" "3.8.1" "dev"]]
+ :plugins ["global" "deps" "java" "package"]
+ :dependencies [["junit" "junit" "3.8.1" "dev"]]
  ]
