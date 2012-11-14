6d059e75bde5bb1ea834c5ef44bf56e96bf455f4
Ludger Solbach <soulman@soulspace.org>
2012-11-14 10:14:22 +0000

    updated module.clj

--- module.clj
+++ module.clj
@@ -1,9 +1,12 @@
 [
  :name "ModelRepository2"
+ :project "org.soulspace.modelling"
  :type "library"
  :version "0.2.0"
  :description "A domain model for the generator framework"
  :vendor "soulspace.org"
- :plugins ["global" "deps" "java" "archive" "publish"]
- :dependencies [["org.junit" "junit" "3.8.1" "dev"]]
+ :plugins ["global" "deps" "mdsd" "java" "package"]
+ :dependencies [["junit" "junit" "3.8.1" "dev"]
+                ["org.soulspace.modelling" "GeneratorConfig" "1.0.0rc3" "generator" "GeneratorConfig" "zip"]
+                ]
  ]
