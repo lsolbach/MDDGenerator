27d936e0a6297ac4c759c28e41085a5cd20e6e36
Ludger Solbach <soulman@soulspace.org>
2012-12-09 20:13:08 +0000

    updated module files

--- module.clj
+++ module.clj
@@ -5,6 +5,6 @@
  :version "0.2.0"
  :description "A repository for UML 1.4/XMI 1.2 files"
  :vendor "soulspace.org"
- :plugins ["global" "deps" "java" "package"]
+ :plugins ["global" "deps" "java" "junit" "package"]
  :dependencies [["junit" "junit" "3.8.1" "dev"]]
  ]
