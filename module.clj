823abeac3bf0a90d2477b851d9a1b791b69ea29e
Ludger Solbach <soulman@soulspace.org>
2013-07-14 13:37:04 +0200

    bumped version to 0.3.0

--- module.clj
+++ module.clj
@@ -2,12 +2,12 @@
  :name "UML14ModelBuilder"
  :project "org.soulspace.modelling"
  :type "library"
- :version "0.2.0"
+ :version "0.3.0"
  :description "A builder for generator models from UML 1.4/XMI 1.2 models"
  :vendor "soulspace.org"
  :plugins ["global" "deps" "java" "package"]
- :dependencies [["org.soulspace.modelling" "XmiRepository2" "0.2.0"]
-                ["org.soulspace.modelling" "ModelRepository2" "0.2.0"]
+ :dependencies [["org.soulspace.modelling" "XmiRepository2" "0.3.0"]
+                ["org.soulspace.modelling" "ModelRepository2" "0.3.0"]
                 ["org.soulspace.modelling" "MDDTemplateLibrary" "1.0.0" "generator" "MDDTemplateLibrary" "zip"]
                 ["junit" "junit" "3.8.1" "dev"]]
  :debug "true"
