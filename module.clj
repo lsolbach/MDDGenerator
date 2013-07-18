395120b079fe8a64b58c7dfbd3cc4683bc72e404
Ludger Solbach <soulman@soulspace.org>
2013-07-18 17:01:20 +0200

    updated dependencies, bumped version to 0.3.0

--- module.clj
+++ module.clj
@@ -4,14 +4,14 @@
  :project-lead "Ludger Solbach"
  :vendor "soulspace.org"
  :type "framework"
- :version "0.2.0"
+ :version "0.3.0"
  :description "Generator framework for model driven software development. Contains generators and ant tasks."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins ["global" "deps" "java" "package"]
- :dependencies [["org.soulspace" "TemplateEngine" "1.0.0"]
-                ["org.soulspace.modelling" "UML14Repository" "0.2.0"]
-                ["org.soulspace.modelling" "ModelRepository2" "0.2.0"]
-                ["org.soulspace.modelling" "UML14ModelBuilder" "0.2.0"]
+ :dependencies [["org.soulspace.template" "TemplateEngine" "1.0.0"]
+                ["org.soulspace.modelling" "UML14Repository" "0.3.0"]
+                ["org.soulspace.modelling" "ModelRepository2" "0.3.0"]
+                ["org.soulspace.modelling" "UML14ModelBuilder" "0.3.0"]
                 ["org.soulspace" "JavaUtilLibrary" "0.2.0"]
                 ["oro" "oro" "2.0.8"]
                 ["org.apache.ant" "ant" "1.8.3" "dev"]
