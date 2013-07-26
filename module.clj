38959355be32de1879ae6f8308cbe0b5882c6a6b
Ludger Solbach <soulman@soulspace.org>
2013-07-26 20:07:14 +0200

    refactored module.clj, refactored type annotation generation, refactored extension generation for interfaces, added annotations on qualified association getters, moved generation context access to model/lib, added java/impl-class include

--- module.clj
+++ module.clj
@@ -1,10 +1,10 @@
 [
- :name "MDDTemplateLibrary"
+ :module "MDDTemplateLibrary"
  :project "org.soulspace.modelling"
- :type "data"
+ :type :data
  :version "1.0.0"
  :description "Templates for model driven software development"
  :author "Ludger Solbach"
  :provider "soulspace.org"
- :plugins ["global" "deps" "package"] 
+ :plugins ["global" "sdeps" "package"] 
  ]
