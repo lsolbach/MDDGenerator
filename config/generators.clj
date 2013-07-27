4506da4c8a61e5ee3d3fc492498340807b353c39
Ludger Solbach <soulman@soulspace.org>
2013-07-27 19:19:29 +0200

    completed refactorings for self hosting, this module can now be generated with the new generator framework, which it is part of

--- config/generators.clj
+++ config/generators.clj
@@ -26,6 +26,14 @@
    :stereotypes "NONE"
    }
   {:element "Class"
+   :template "java/class"
+   :includes ["lib" "model/lib" "java/lib" "java/class" "uml14/lib" "uml14/mof/primitive"]
+   :subdir "src"
+   :extension "java"
+   :baseNamespace "org.soulspace.modelling.uml14.primitives"
+   :stereotypes "primitive"
+   }
+  {:element "Class"
    :template "uml14/mof/enum"
    :includes ["lib" "model/lib" "java/lib" "java/class" "uml14/lib"]
    :subdir "src"
@@ -33,31 +41,23 @@
    :baseNamespace "org.soulspace.modelling.uml14.types"
    :stereotypes "enumeration"
    }
-  {:element "Class"
-   :template "uml14/mof/primitive"
-   :includes ["lib" "model/lib" "java/lib" "java/class" "uml14/lib"]
-   :subdir "src"
-   :extension "java"
-   :baseNamespace "org.soulspace.modelling.uml14.primitives"
-   :stereotypes "primitive"
-   }
   {:element "Model"
-   :template "uml14/mof/repository"
+   :template "uml14/mof/repository" ; TODO switch to java/class?
    :includes ["lib" "model/lib" "java/lib" "java/class" "uml14/lib"]
    :subdir "src"
+   :baseNamespace "org.soulspace.modelling.uml14.impl"
    :baseName "Uml14RepositoryImpl"
    :extension "java"
-   :baseNamespace "org.soulspace.modelling.uml14.impl"
    }
   {:element "Model"
-   :template "uml14/xmi/1.2/schema"
-   :includes ["lib" "model/lib"]
+   :template "uml14/xmi/1.2/schema" ; TODO use xml/xml with xml/schema include?
+   :includes ["lib" "model/lib" "uml14/lib"]
    :subdir "uml"
-   :basename "uml14_xmi12"
+   :baseName "uml14_xmi12"
    :extension "xsd"
    }
   {:element "Model"
-   :template "uml14/xmi/1.2/reader"
+   :template "uml14/xmi/1.2/reader" ; TODO switch to java/class?
    :includes ["lib" "model/lib" "java/lib" "java/class" "uml14/lib"]
    :subdir "src"
    :baseName "Xmi12ReaderImpl"
@@ -65,25 +65,11 @@
    :baseNamespace "org.soulspace.modelling.uml14.impl"
    }
   {:element "Model"
-   :template "uml14/xmi/1.2/writer"
+   :template "uml14/xmi/1.2/writer" ; TODO switch to java/class?
    :includes ["lib" "model/lib" "java/lib" "java/class" "uml14/lib"]
    :subdir "src"
    :baseName "Xmi12WriterImpl"
    :extension "java"
    :baseNamespace "org.soulspace.modelling.uml14.impl"
-   }
-  {:element "Model"
-   :template "uml14/xmi/1.2/clj-dsl"
-   :includes ["lib" "model/lib"]
-   :subdir "clj"
-   :baseName "xmi12"
-   :extension "clj"
-   }
-  {:element "Model"
-   :template "uml14/xmi/1.2/clj-uml14-dsl"
-   :includes ["lib" "model/lib"]
-   :subdir "clj"
-   :baseName "uml14"
-   :extension "clj"
    }]
  }
