8a922b6c168fc13e3eb85f2dfb46e0bcb30fbd1d
Ludger Solbach <soulman@soulspace.org>
2013-07-30 21:00:11 +0200

    bumped version to 0.4.0, refactored sef hosting templates and generator config, updated module files

--- module.clj
+++ module.clj
@@ -2,18 +2,20 @@
  :module "UML14ModelBuilder"
  :project "org.soulspace.modelling"
  :type :library
- :version "0.3.0"
+ :version "0.4.0"
  :description "A builder for generator models from UML 1.4/XMI 1.2 models"
  :vendor "soulspace.org"
  :plugins ["global" "sdeps" "mdsd" "java" "package"]
- :dependencies [[["org.soulspace.modelling" "UML14Repository" "0.3.0"]]
-                [["org.soulspace.modelling" "ModelRepository2" "0.3.0"]]
+ :dependencies [[["org.soulspace.modelling" "UML14Repository" "0.4.0"]]
+                [["org.soulspace.modelling" "MDDRepository" "0.4.0"]]
                 [["org.soulspace.modelling" "ArgoUMLProfileLibrary" "1.0.0" "ArgoUMLProfileLibrary" "zip"] :generator]
                 [["org.soulspace.modelling" "MDDTemplateLibrary" "1.0.0" "MDDTemplateLibrary" "zip"] :generator]
                 [["junit" "junit" "3.8.1"] :dev]]
  :mdsd-config-dir "config"
  :mdsd-template-path "${lib-generator-dir}/templates:templates"
  :mdsd-std-profiles ["argouml/default-uml14"]
+ :mdsd-model-dir "../MDDRepository/model/"
+ :mdsd-model-name "MDDRepository"
  :log-level :debug
  :debug "true"
  ]
