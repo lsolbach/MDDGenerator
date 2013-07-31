dd8916524e0af8ce909eff1d371ab85b93d3b000
Ludger Solbach <soulman@soulspace.org>
2013-07-31 09:49:14 +0200

    updated module files

--- module.clj
+++ module.clj
@@ -7,25 +7,20 @@
  :version "0.4.0"
  :description "Generator framework for model driven software development. Contains generators and ant tasks."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "sdeps" "mdsd" "java" "package"]
- :dependencies [[["org.soulspace.template" "TemplateEngine" "1.0.0"]]
+ :plugins ["global" "sdeps" "depsdot" "mdsd" "java" "package"]
+ :dependencies [[["org.soulspace.template" "TemplateEngine" "1.0.1"]]
                 [["org.soulspace.common" "JavaUtilLibrary" "0.3.0"]]
                 [["org.soulspace.modelling" "ArgoUMLProfileLibrary" "1.0.0" "ArgoUMLProfileLibrary" "zip"] :generator]
                 [["org.soulspace.modelling" "MDDTemplateLibrary" "1.0.0" "MDDTemplateLibrary" "zip"] :generator]
 ;                [["org.soulspace.modelling" "MDDRepository" "0.4.0" "MDDRepository" "xmi"] "model"]
-                [["org.soulspace.modelling" "MDDRepository" "0.4.0"]]
-                [["org.soulspace.modelling" "UML14Repository" "0.4.0"]]
                 [["org.soulspace.modelling" "UML14ModelBuilder" "0.4.0"]]
-                [["oro" "oro" "2.0.8"]]
                 [["org.apache.ant" "ant" "1.8.3"] :dev]
-                [["org.apache.ant" "ant-launcher" "1.8.3"] :dev]
                 [["org.apache.ant" "ant-testutil" "1.8.3"] :dev]
                 [["junit" "junit" "3.8.1"] :dev]]
  :mdsd-config-dir "./config"
- :mdsd-config-file "generators.clj"
  :mdsd-template-path "${lib-generator-dir}/templates:templates"
  :mdsd-model-dir "../MDDRepository/model"
  :mdsd-model-name "MDDRepository"
  :mdsd-std-profiles ["argouml/default-uml14"]
- :log-level :debug
+ :log-level :info
  ]
