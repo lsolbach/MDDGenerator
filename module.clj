0b52d58be81bb1f8bbbfce8c3f48039bee71b056
Ludger Solbach <soulman@soulspace.org>
2013-08-03 08:06:15 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -6,5 +6,5 @@
  :description "The ArgoUMLProfileLibrary provides the default profiles of ArgoUML for external use"
  :author "ArgoUML Team, Ludger Solbach"
  :provider "soulspace.org"
- :plugins ["global" "sdeps" "package"] 
+ :plugins ["global" "dependencies" "package"] 
  ]
