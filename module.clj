f998eca9bdea4883a46e07f5ef0c911905b8afa7
Ludger Solbach <soulman@soulspace.org>
2013-08-05 07:31:11 +0200

    updated module files, added copyright notice in source files, added license.txt

--- module.clj
+++ module.clj
@@ -5,7 +5,7 @@
  :version "0.4.0"
  :description "The UML14Repository provides a repository for UML 1.4 models and importers/exporters for XMI 1.2 files"
  :provider "soulspace.org"
- :plugins ["global" "dependencies" "mdsd" "java" "junit" "package"]
+ :plugins ["global" "dependencies" "mdsd" "java" "junit" "package" "release"]
  :dependencies [[["junit" "junit" "3.8.1"] :dev]
                 [["org.soulspace.modelling" "ArgoUMLProfileLibrary" "1.0.0" "ArgoUMLProfileLibrary" "zip"] :generator]
                 [["org.soulspace.modelling" "MDDTemplateLibrary" "1.0.0" "MDDTemplateLibrary" "zip"] :generator]]
