334e2d4308ee622cad67f5699b532009aa940aba
Ludger Solbach <soulman@soulspace.org>
2013-11-23 12:32:29 +0100

    version bump to 0.5.0

--- module.clj
+++ module.clj
@@ -4,16 +4,16 @@
  :project-lead "Ludger Solbach"
  :provider "soulspace.org"
  :type :framework
- :version "0.4.0"
+ :version "0.5.0"
  :description "Generator framework for model driven software development. Contains generators and ant tasks."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "sdependencies" "mdsd" "java" "junit" "package" "release"]
+ :plugins ["global" "dependencies" "mdsd" "java" "junit" "package" "release"]
  :dependencies [[["org.soulspace.template" "TemplateEngine" "1.0.1"]]
                 [["org.soulspace.common" "JavaUtilLibrary" "0.3.0"]]
                 [["org.soulspace.modelling" "ArgoUMLProfileLibrary" "1.0.0" "ArgoUMLProfileLibrary" "zip"] :generator]
                 [["org.soulspace.modelling" "MDDTemplateLibrary" "1.0.0" "MDDTemplateLibrary" "zip"] :generator]
-;                [["org.soulspace.modelling" "MDDRepository" "0.4.0" "MDDRepository" "xmi"] "model"]
-                [["org.soulspace.modelling" "UML14ModelBuilder" "0.4.0"]]
+;                [["org.soulspace.modelling" "MDDRepository" "0.5.0" "MDDRepository" "xmi"] "model"]
+                [["org.soulspace.modelling" "UML14ModelBuilder" "0.5.0"]]
                 [["org.apache.ant" "ant" "1.8.3"] :dev]
                 [["org.apache.ant" "ant-testutil" "1.8.3"] :dev]
                 [["junit" "junit" "3.8.1"] :dev]]
