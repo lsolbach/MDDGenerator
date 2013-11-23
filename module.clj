8b5181799859a12d50b8c57f0b405675c7b47188
Ludger Solbach <soulman@soulspace.org>
2013-11-23 11:56:25 +0100

    small enhancements and doc updates, version bump to 1.0.1

--- module.clj
+++ module.clj
@@ -2,7 +2,7 @@
  :module "MDDTemplateLibrary"
  :project "org.soulspace.modelling"
  :type :data
- :version "1.0.0"
+ :version "1.0.1"
  :description "Templates for model driven software development"
  :author "Ludger Solbach"
  :provider "soulspace.org"
