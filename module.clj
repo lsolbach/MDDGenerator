b73a61b658052436fc5828063fd1362ff2cd7d55
Ludger Solbach <soulman@soulspace.org>
2014-02-28 23:59:41 +0100

    enhanced uml diagram generation, updated module.clj

--- module.clj
+++ module.clj
@@ -2,9 +2,11 @@
  :module "MDDTemplateLibrary"
  :project "org.soulspace.modelling"
  :type :data
- :version "1.0.1"
+ :version "1.0.2"
  :description "Templates for model driven software development"
  :author "Ludger Solbach"
  :provider "soulspace.org"
- :plugins ["global" "dependencies" "package" "release"] 
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]] 
  ]
