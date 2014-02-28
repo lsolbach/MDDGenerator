dc76ca319b5b0efa99fc6ad9a6e8d2ad2097c3ff
Ludger Solbach <soulman@soulspace.org>
2014-03-01 00:13:36 +0100

    updated module.clj, updated model

--- module.clj
+++ module.clj
@@ -5,13 +5,17 @@
  :version "0.5.0"
  :description "The UML14Repository provides a repository for UML 1.4 models and importers/exporters for XMI 1.2 files"
  :provider "soulspace.org"
- :plugins ["global" "dependencies" "mdsd" "java" "junit" "package" "release"]
- :dependencies [[["junit" "junit" "3.8.1"] :dev]
-                [["org.soulspace.modelling" "ArgoUMLProfileLibrary" "1.0.0" "ArgoUMLProfileLibrary" "zip"] :generator]
-                [["org.soulspace.modelling" "MDDTemplateLibrary" "1.0.0" "MDDTemplateLibrary" "zip"] :generator]]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/MDDGeneratorPlugin"]
+           ["org.soulspace.baumeister/JavaPlugin"]
+           ["org.soulspace.baumeister/JUnitPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["junit" "junit" "3.8.1" :dev]
+                ["org.soulspace.modelling/ArgoUMLProfileLibrary, 1.0.0, ArgoUMLProfileLibrary, zip" :generator]
+                ["org.soulspace.modelling/MDDTemplateLibrary, 1.0.0, MDDTemplateLibrary, zip" :generator]]
  :mdsd-config-dir "./config"
  :mdsd-template-path "${lib-generator-dir}/templates:templates"
  :mdsd-std-profiles ["argouml/default-uml14"]
- :log-level "debug"
  :debug "true"
  ]
