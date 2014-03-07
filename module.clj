b9d2c7855f20de48dac2cd03e66cf6aad0b770b7
Ludger Solbach <soulman@soulspace.org>
2014-03-07 09:33:36 +0100

    updated module.clj, updated model

--- module.clj
+++ module.clj
@@ -5,10 +5,15 @@
  :version "0.5.0"
  :description "The module MDDRepository contains the model repository of the MDD Generator Framework to facilitate code generation."
  :provider "soulspace.org"
- :plugins ["global" "dependencies" "mdsd" "java" "package" "release"]
- :dependencies [[["junit" "junit" "3.8.1"] :dev]
-                [["org.soulspace.modelling" "ArgoUMLProfileLibrary" "1.0.0" "ArgoUMLProfileLibrary" "zip"] :generator]
-                [["org.soulspace.modelling" "MDDTemplateLibrary" "1.0.0" "MDDTemplateLibrary" "zip"] :generator]]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/MDDGeneratorPlugin"]
+           ["org.soulspace.baumeister/JavaPlugin"]
+           ["org.soulspace.baumeister/JUnitPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["junit/junit, 3.8.1" :dev]
+                ["org.soulspace.modelling/ArgoUMLProfileLibrary, 1.0.0, ArgoUMLProfileLibrary, zip" :generator]
+                ["org.soulspace.modelling/MDDTemplateLibrary, 1.0.0, MDDTemplateLibrary, zip" :generator]]
  :mdsd-config-dir "./config"
 ; :mdsd-config-file "generators.clj"
  :mdsd-template-path "${lib-generator-dir}/templates:templates"
