f3d49465a33d694c127b4e0371b948f483f114e4
Ludger Solbach <soulman@soulspace.org>
2014-03-17 08:17:32 +0100

    updated module.clj

--- module.clj
+++ module.clj
@@ -17,10 +17,10 @@
                 ["org.soulspace.modelling/ArgoUMLProfileLibrary, 1.0.0, ArgoUMLProfileLibrary, zip" :generator]
                 ["org.soulspace.modelling/MDDTemplateLibrary, 1.0.1, MDDTemplateLibrary, zip" :generator]
                 ["junit/junit, 3.8.1" :dev]]
- :mdsd-config-dir "config"
- :mdsd-template-path "${lib-generator-dir}/templates:templates"
- :mdsd-std-profiles ["argouml/default-uml14"]
- :mdsd-model-dir "../MDDRepository/model/"
- :mdsd-model-name "MDDRepository"
- :debug "true"
+ :mddgenerator-config-dir "config"
+ :mddgenerator-template-path "${lib-generator-dir}/templates:templates"
+ :mddgenerator-std-profiles ["argouml/default-uml14"]
+ :mddgenerator-model-dir "../MDDRepository/model/"
+ :mddgenerator-model-name "MDDRepository"
+ :compile-debug "true"
  ]
